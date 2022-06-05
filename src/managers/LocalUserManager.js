ccc642fb2ae32c23673e515ce57423b171c8a62e
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-06-05 22:29:01 +0300

    bump

--- src/managers/LocalUserManager.js
+++ src/managers/LocalUserManager.js
@@ -15,12 +15,12 @@ module.exports = class LocalUserManager {
    * 
    * @returns {Promise<{MatchPercents: number, User: import("../structures/User")}[]>}
    */
-  async FetchMatches(Offset, Limit, Sorted=false, LowerLimit = 0.49) {
+  async FetchMatches(Offset, Limit, Sorted=false, LowerPoint = 0.49) {
     let data = await this.Client.AwaitResponse("LocalUser:Get:Matches", {
       Offset,
       Limit,
       Sorted,
-      LowerLimit
+      LowerPoint
     }, false);
 
     return await quickMap(data, async i => {

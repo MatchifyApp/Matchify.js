ccf72b03843a35191c0ad2145db23474fb549f8d
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-06-07 15:39:23 +0300

    wowxd

--- src/managers/SocketManager.js
+++ src/managers/SocketManager.js
@@ -62,7 +62,7 @@ class SocketManager {
    */
   AwaitResponse(eventName, data = {}) {
     return new Promise((resolve, reject) => {
-      this.Socket.emit(eventName, data, (x) => x.ok ? resolve(x.data) : reject(x.error))
+      this.Socket.emit(eventName, data, (x) => x.ok ? resolve(x.data) : reject(new Error("API Error: " + x.error)))
     })
   }
 
